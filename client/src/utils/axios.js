17c1466b84ac7d5e5a6af3b74a5a9363a739922e
Chirag Panwar <125826529+ChiR24@users.noreply.github.com>
2025-02-17 23:13:35 +0530

    fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true,
+    baseURL: process.env.REACT_APP_API_URL || 'https://alfredtask-through-internshala.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     }
