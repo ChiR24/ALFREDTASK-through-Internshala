ee21d7cac8f0c27d5e34beb551f6113c99b5464c
Chirag Panwar <125826529+ChiR24@users.noreply.github.com>
2025-02-18 19:00:18 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import axios from 'axios';
 
 // Set base URL for axios
-axios.defaults.baseURL = 'https://alfredtask-through-internshala.onrender.com/api';
+axios.defaults.baseURL = 'https://alfredtask-through-internshala.onrender.com/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
